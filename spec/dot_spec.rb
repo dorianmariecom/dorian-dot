eae39651f4981949f52a71d7e5b56af99f325b4a
Dorian Marié <dorian@dorianmarie.com>
2024-04-10 04:26:33 +0200

    fix nested directories

--- spec/dot_spec.rb
+++ spec/dot_spec.rb
@@ -4,12 +4,17 @@ require "spec_helper"
 require "securerandom"
 require "tmpdir"
 require "tempfile"
+require "fileutils"
 
 RSpec.describe "dot" do
   it "works" do
     tempdir = Dir.mktmpdir
     tempfile = Tempfile.create(SecureRandom.hex, tempdir)
+    nested_tempdir = Dir.mktmpdir(SecureRandom.hex, tempdir)
+    nested_tempfile = Tempfile.create(SecureRandom.hex, nested_tempdir)
     homefile = "#{Dir.home}/#{File.basename(tempfile)}"
+    nested_homedir = "#{Dir.home}/#{File.basename(nested_tempdir)}"
+    nested_homefile = "#{nested_homedir}/#{File.basename(nested_tempfile)}"
 
     Dir.chdir(tempdir) do
       `git init && git add . && git commit -m "test from dorian-dot"`
@@ -19,21 +24,12 @@ RSpec.describe "dot" do
 
     expect(File.symlink?(homefile)).to eq(true)
     expect(File.readlink(homefile)).to eq(tempfile.path)
+    expect(File.symlink?(nested_homefile)).to eq(true)
+    expect(File.readlink(nested_homefile)).to eq(nested_tempfile.path)
+    expect(File.directory?(nested_homedir)).to eq(true)
   ensure
-    begin
-      File.delete(homefile)
-    rescue StandardError
-      nil
-    end
-    begin
-      File.delete(tempfile)
-    rescue StandardError
-      nil
-    end
-    begin
-      Dir.delete(tempdir)
-    rescue StandardError
-      nil
-    end
+    File.delete(homefile) rescue StandardError
+    FileUtils.rm_r(nested_homedir) rescue StandardError
+    FileUtils.rm_r(tempdir) rescue StandardError
   end
 end
